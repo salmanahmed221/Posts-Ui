300ada0dc1a21484a4584b5c6066a1bf0f3fa648
unknown <salmanahmed121221@gmail.com>
2023-07-10 13:45:20 +0500

    Fix my mistake

--- tailwind.config.js
+++ tailwind.config.js
@@ -12,9 +12,6 @@ module.exports = {
         'gradient-conic':
           'conic-gradient(from 180deg at 50% 50%, var(--tw-gradient-stops))',
       },
-      fontFamily: {
-        Khula: "Khula",
-      },
       colors: {
         gray1: "#000000",
         black1: "#3D3C42",
